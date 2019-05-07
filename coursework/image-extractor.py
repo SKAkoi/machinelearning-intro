dc6f0f607f8ecb3c0e9b6612d5036242bb81400c
Kwaku Akoi <akoikwaku@gmail.com>
2019-05-07 07:25:08 -0500

    Added missing code for unzipping validation set

--- coursework/image-extractor.py
+++ coursework/image-extractor.py
@@ -5,4 +5,7 @@ import zipfile
 local_zip = '/tmp/horse-or-human.zip'
 zip_ref = zipfile.ZipFile(local_zip, 'r')
 zip_ref.extractall('/tmp/horse-or-human')
+local_zip = '/tmp/validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/validation-horse-or-human')
 zip_ref.close()
